291e1a68bd5c2760820ae4aac81f1972a924586f
samuraii <7thsamurai@mail.ru>
2018-03-31 19:06:16 +0000

    Get only online friends

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -2,7 +2,7 @@ import vk
 import getpass
 
 
-APP_ID = '6113971'  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = '6113971'
 
 
 def get_user_login():
@@ -19,12 +19,17 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session, v='5.73')
-    all_friends = api.friends.get(fields='online')['items']
-    online_friends = filter(lambda x: x['online'] == 1, all_friends)
-    return list(online_friends)
-    
+    online_friends_ids = api.friends.getOnline()
+    friends_online = api.users.get(
+        user_ids=online_friends_ids,
+        fields="first_name,last_name",
+    )
+    return list(friends_online)
+
+
 def output_friends_to_console(friends_online):
     print('Now online:')
     for friend in friends_online:
