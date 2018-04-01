30d54491990e662e0e212185c7f2c776df0e432a
campside <chirkovv.ma@yandex.ru>
2018-04-01 18:35:06 +0300

    Правки по ревью
    1) Убрал свой логи и пароль (забыл, пришлось менять пароль от вк). 2) Сделал версию api параметром по умолчанию. 3) Добавил try..except для неверного логина и пароля

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,5 +1,6 @@
 import vk
 import getpass
+import sys
 
 
 APP_ID = '6113971'
@@ -14,20 +15,20 @@ def get_user_password():
     return user_password
 
 
-def get_online_friends(login, password):
+def get_online_friends(login, password, api_version='5.73'):
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
         user_password=password,
         scope='friends'
     )
-    api = vk.API(session, v='5.73')
+    api = vk.API(session, v=api_version)
     online_friends_ids = api.friends.getOnline()
     friends_online = api.users.get(
         user_ids=online_friends_ids,
         fields="first_name,last_name",
     )
-    return list(friends_online)
+    return friends_online
 
 
 def output_friends_to_console(friends_online):
@@ -37,7 +38,10 @@ def output_friends_to_console(friends_online):
 
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
+    try:
+        login = get_user_login()
+        password = get_user_password()
+        friends_online = get_online_friends(login, password)
+    except vk.exceptions.VkAuthError:
+        sys.exit('Ошибка авторизации, неверный логин или пароль.')
     output_friends_to_console(friends_online)
